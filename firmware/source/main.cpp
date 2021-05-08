379d6824bb1a7bdfa109eb1fcb69f3caa2bc433c
Fabian Wührer <fabian.wuehrer@tum.de>
2021-05-08 17:45:56 +0200

    added print_packet and fixed IRQHandler

--- firmware/source/main.cpp
+++ firmware/source/main.cpp
@@ -7,21 +7,35 @@
 MicroBit uBit;
 Link* usbLink;
 
+void print_packet(uint8_t* packet, int size, uint8_t flags = 0xff){
+	if (flags != 0xff){
+		uBit.display.print(flags);
+		wait_ms(500);
+		uBit.display.print(' ');
+	}
+	for (int i = 0; i < size + 1; i++)
+	{
+		wait_ms(500);
+		uBit.display.print((char) packet[i]);
+	}
+	uBit.display.clear();
+}
+
 extern "C" void RADIO_IRQHandler(void)
 {
+	usbLink->sendPacket(TEST, (uint8_t*) "IRQ_HANDLER_START", strlen((char*) "IRQ_HANDLER_START"), 0);
+
 	if (NRF_RADIO->EVENTS_READY) {
 		NRF_RADIO->EVENTS_READY = 0;
+		usbLink->sendPacket(TEST, (uint8_t*) "IRQ EVENTS_READY", strlen((char*) "IRQ EVENTS_READY"), 0);
 		NRF_RADIO->TASKS_START = 1;
 	}
 
 	if (NRF_RADIO->EVENTS_END) {
-        	NRF_RADIO->EVENTS_END = 0;
+        NRF_RADIO->EVENTS_END = 0;
+		usbLink->sendPacket(TEST, (uint8_t*) "IRQ EVENTS_END", strlen((char*) "IRQ EVENTS_END"), 0);
 
-		char buf[] = "IRQ";
-		usbLink->sendPacket(TEST, (uint8_t*) buf, 3, 0);
-		uBit.display.scroll("IRQ");
-
-		/* continue TODO only for receiver, sender should not repeat */
+		// continue TODO only for receiver, sender should not repeat
 		//NRF_RADIO->TASKS_START = 1;
 	}
 }
@@ -33,37 +47,33 @@ void dispatchCMD(T_CMD cmd, uint8_t* packet, int size, uint8_t flags)
 	switch (cmd)
 	{
 	case TEST:
-		uBit.display.print(flags);
-		for (int i = 0; i < size; i++)
-		{
-			wait_ms(1000);
-			uBit.display.print((char) packet[i]);
-		}
-		uBit.display.clear();
-		buf[0] = (uint8_t) 'T';
-		buf[1] = (uint8_t) 'E';
-		buf[2] = (uint8_t) 'S';
-		buf[3] = (uint8_t) 'T';
-		usbLink->sendPacket(TEST, buf, 4, 0);
+		strcpy((char*) buf, "TESTING");
+		usbLink->sendPacket(TEST, buf, strlen((char*) buf), 0);
+		print_packet(packet, size, flags);
+		usbLink->sendPacket(cmd, packet, size, flags);
 		break;
 	case SEND:
+		strcpy((char*) buf, "SENDING");
+		usbLink->sendPacket(TEST, buf, strlen((char*) buf), 0);
+		usbLink->sendPacket(cmd, packet, strlen((char*) packet), flags);
 		radio_send((uint32_t) 0x554433, 11, (uint8_t*) packet, size);
 		strcpy((char*) buf, "SENT");
 		usbLink->sendPacket(TEST, buf, strlen((char*) buf), 0);
 		break;
 	case RECV:
-		radio_receive((uint32_t) 0xAABBCC, 11, buf);
 		strcpy((char*) buf, "RECEIVING");
 		usbLink->sendPacket(TEST, buf, strlen((char*) buf), 0);
+		strcpy((char*) buf, "");
+		radio_receive((uint32_t) 0xAABBCC, 11, buf);
+		//print_packet(buf, size, flags);
+		usbLink->sendPacket(cmd, buf, strlen((char*) buf), flags);
 		break;
 	default:
+		strcpy((char*) buf, "NOPE");
+		usbLink->sendPacket(TEST, buf, strlen((char*) buf), 0);
 		uBit.display.scroll("UNKNOWN CMD");
 		uBit.display.print(cmd);
-		buf[0] = (uint8_t) 'N';
-		buf[1] = (uint8_t) 'O';
-		buf[2] = (uint8_t) 'P';
-		buf[3] = (uint8_t) 'E';
-		usbLink->sendPacket(TEST, buf, 4, 0);
+		usbLink->sendPacket(cmd, packet, size, flags);
 		break;
 	}
 }
